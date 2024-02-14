84d81d52ad3383a12f1703a1816e1ebda5cab3b3
cover <98329722+nanshaws@users.noreply.github.com>
2024-02-14 13:34:25 +0800

    解决了中文乱码问题
    解决了中文乱码问题

--- processManager/src/main/java/org/example/processmanager/utils/ServiceManager.java
+++ processManager/src/main/java/org/example/processmanager/utils/ServiceManager.java
@@ -5,6 +5,7 @@ import org.example.processmanager.pojo.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -23,7 +24,7 @@ public class ServiceManager {
             // 执行 sc query 命令来获取服务列表
             Process process = new ProcessBuilder("cmd", "/c", "sc", "query").start();
             // 读取命令输出
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), Charset.forName("GBK")));
             String line;
             while ((line = reader.readLine()) != null) {
                 Service service = new Service();
